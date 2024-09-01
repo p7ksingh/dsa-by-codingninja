3c437e69d61529cff8ceb69d4a9281d4f07e0355
developer <developer@localhost>
2024-09-01 13:04:03 +0530

    simple intrest calculater

---
+++ src/com/p1/basicDSA/SimpleIntrestCalcuter.java
@@ -0,0 +1,13 @@
+package com.p1.basicDSA;
+
+public class SimpleIntrestCalcuter {
+public static void main(String[] args) {
+	int p=1000;
+	int r=10;
+	int t=5;
+	
+	int si =(p*r*t)/100;
+	System.out.println("Simple intrest is : "+si);
+
+}
+}
