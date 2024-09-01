9deeb2a4fd0ecfd18fc694eb91fcad0cc784c952
developer <p7ksingh@gmail.com>
2024-09-01 15:20:29 +0530

    arthemetic operatation

---
+++ src/com/p1/basicDSA/FahToCel.java
@@ -0,0 +1,28 @@
+package com.p1.basicDSA;
+
+import java.util.Scanner;
+
+public class FahToCel {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int f =sc.nextInt();
+		
+//		int cel=(5/9)*(f-32);
+//		System.out.println("cel value is :"+cel);//0
+		
+	//	int cel=(5*(f-32))/9;
+		
+//		int cel =(int) ((5.0/9)*(f-32));
+//		System.out.println("cel value is :"+cel);
+		System.out.println(5+9);
+		System.out.println(9-5);
+		System.out.println(5*9);
+		System.out.println(5/9);
+		System.out.println(5.0/9);
+		System.out.println(5%9);
+		System.out.println('a');
+		System.out.println('a'+'b');
+	}
+
+}
