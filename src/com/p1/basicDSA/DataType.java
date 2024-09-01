67da356607bb0838b5e86abe768d15ccfa6bb5e1
developer <p7ksingh@gmail.com>
2024-09-01 22:15:21 +0530

    Data type casting

---
+++ src/com/p1/basicDSA/DataType.java
@@ -0,0 +1,25 @@
+package com.p1.basicDSA;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		char c ='c';
+		System.out.println(c+3);//102 bez + indicate integer
+		int i=c+10;
+		System.out.println(i);
+		i=c;
+		//char ch=i;
+		long l=i;
+		//i=l;
+		double d=i;
+		//i=d;
+		float f=1.23f;
+		int j=100;
+		
+		long a=987612345645l;
+		int b =(int)a;
+		System.out.println(b);
+
+	}
+
+}
