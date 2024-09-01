219a70ba9cc109d1d8e6bca6f4b05b88788a619a
developer <p7ksingh@gmail.com>
2024-09-01 13:50:28 +0530

    Scanner added

---
+++ src/com/p1/basicDSA/ScannerClass.java
@@ -0,0 +1,13 @@
+package com.p1.basicDSA;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	int a =scan.nextInt();
+	int b =scan.nextInt();
+	int j= a+b;
+	System.out.println("total sum is : "+j);
+}
+}
