f85584ce3a73cdd40ae4553062379b0794d06bfe
developer <p7ksingh@gmail.com>
2024-09-02 22:10:58 +0530

    square patter added

---
+++ src/if_else/SquarePattern.java
@@ -0,0 +1,23 @@
+package if_else;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		int n=4;
+		int i=1;
+		
+		
+		while(i<=n) {
+			int j=1;
+			while(j<=n) {
+				System.out.print("* ");
+				j++;
+				
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
