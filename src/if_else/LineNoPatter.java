028243445b10c3083555018617c5373f5bfae37b
developer <p7ksingh@gmail.com>
2024-09-02 22:14:44 +0530

    Line no print

---
+++ src/if_else/LineNoPatter.java
@@ -0,0 +1,21 @@
+package if_else;
+
+public class LineNoPatter {
+
+	public static void main(String[] args) {
+		int n=4;
+		int i=1;
+		while(i<=n) {
+			int j=1;
+			while(j<=n) {
+				System.out.print(i+" ");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+		
+
+	}
+
+}
